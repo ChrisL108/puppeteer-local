057c65c2de499999e5295131bceab0389d3039de
Christopher Lacaille <chrislacaille5@gmail.com>
2020-10-25 21:07:09 -0500

    testing; before migrating new code over

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ import fetch from "isomorphic-unfetch";
 import fs from "fs";
 
 (async () => {
-  let currPageUrl;
   const browser = await puppeteer.launch({
-    // userDataDir: "./puppeteer",
+    userDataDir: "./puppeteer",
     // args: ["--no-sandbox"],
     // dumpio: true,
     headless: false,
@@ -15,24 +14,7 @@ import fs from "fs";
     timeout: 0,
   });
   let page = await browser.newPage();
-
-  /**
-   ** EVENT HANDLERS
-   */
-  // browser.on("targetchanged", (e) => {
-  //   console.log("browser target changed, e: ", e);
-  //   currPageUrl = e.url();
-  // });
-
-  // await page.setRequestInterception(true);
-  let requestUrl;
-  page.on("request", (request) => {
-    requestUrl = request.url();
-    if (requestUrl.includes("pdf"))
-      console.log('on("request"), pdf request.url()');
-    // return;
-  });
-
+  let pageContent;
   try {
     // await page._client.send("Page.setDownloadBehavior", {
     //   behavior: "allow",
@@ -47,19 +29,18 @@ import fs from "fs";
     await page.type("input[name=password]", process.env.domo_password);
     await page.click("button[name=submit]");
     await page.waitForSelector("div[app-info=appInfo]");
-    await page.goto(process.env.domo_page_insights, { timeout: 45000 });
+    await page.goto(process.env.customerInsights);
 
     console.log("finding dropdown button for filters");
 
     const TEST_PARTNER = "CDW Logistics";
-    const filterDropdownXPath =
+    const filterPartnerNameXPath =
       "//fb-tile[1]/div[1]/span[contains(., 'Partner')]";
     const filterPartnerNameSelector =
       "input[placeholder='Filter by...']:nth-child(1)";
 
     const filtersClose = "i[class='icon-chevron-up']";
     const filtersOpen = "i.ng-scope.icon-chevron-down";
-    // const filtersOpen = "i[class='icon-chevron-down']";
 
     const filtersOpenButton = await page.waitForSelector(filtersOpen);
     // const filtersCloseButton = await page.waitForSelector(filtersClose, {
@@ -72,29 +53,37 @@ import fs from "fs";
     // }
     if (filtersOpenButton) {
       console.log("Opening filters...");
-      const openFilters = await page.waitForSelector(filtersOpen);
-      openFilters.click();
-      await page.waitForTimeout(2500);
+      try {
+        const openFilters = await page.waitForSelector(filtersOpen);
+        openFilters.click();
+      } catch (error) {
+        console.log(
+          'ERROR FINDING "open filters" button: ',
+          JSON.stringify(error)
+        );
+      }
     }
+    await page.waitForTimeout(2500);
 
     // TODO REMOVE ------------------
     // await browser.close();
     // return;
     // TODO REMOVE ------------------
 
-    const partnerFilter = await page.waitForXPath(filterDropdownXPath, {
+    console.log("Opening Partner Name filter...");
+    const partnerFilter = await page.waitForXPath(filterPartnerNameXPath, {
       // timeout: 0,
     });
     // const partnerFilter = await page.$x(filterDropdownXPath)
     if (partnerFilter) {
       await partnerFilter.click();
-      console.log("partner filter dropdown clicked");
+      console.log("Selecting Partner Name text field...");
       const filterInputName = await page.waitForSelector(
         filterPartnerNameSelector
       );
       // const filterInputName = await page.waitForXPath(filterPartnerNameXPath)
       if (filterInputName) {
-        console.log("filter input box found");
+        console.log("Typing in Partner Name text field...");
         await page.type(filterPartnerNameSelector, TEST_PARTNER);
 
         // wait for filtered results to populate
@@ -114,6 +103,96 @@ import fs from "fs";
         );
         await shareIcon.click();
 
+        // // TODO remove early exit
+        // // TODO remove early exit
+        // res.send('done')
+        // await browser.close()
+        // return
+        // // TODO remove early exit
+        // // TODO remove early exit
+        try {
+          console.log("starting CDP session 1...");
+          // handleCdpSession(page, 1, resolve, browser)
+          let body;
+          const client = await page.target().createCDPSession();
+          await client.send("Fetch.enable", {
+            patterns: [
+              {
+                urlPattern: "*",
+                requestStage: "Response",
+              },
+            ],
+          });
+          await client.on("Fetch.requestPaused", async (reqEvent) => {
+            const {
+              requestId,
+              request: { url },
+              resourceType,
+            } = reqEvent;
+            let responseHeaders = reqEvent.responseHeaders || [];
+            let contentType = "";
+            // console.log('reqEvent: ', reqEvent)
+            for (let element of responseHeaders) {
+              if (element.name.toLowerCase() === "content-type") {
+                console.log("content-type FOUND");
+                contentType = element.value;
+                break;
+              }
+            }
+            // console.log('contentType: ', contentType)
+            // console.log('contentType.includes("application/pdf"): ', contentType.includes('application/pdf'))
+
+            // if (true) {
+            if (url.includes("/export")) {
+              // if (contentType.includes('application/pdf')) {
+              // console.log('resourceType: ', resourceType)
+              // console.log('responseHeaders: ', Object.keys(responseHeaders))
+              // console.log('PDF Request found, reqEvent: ', reqEvent)
+              responseHeaders.push({
+                name: "content-disposition",
+                value: "attachment",
+              });
+
+              const responseObj = await client.send("Fetch.getResponseBody", {
+                requestId,
+              });
+              console.log(
+                "PDF responseObj (first 200) BEFORE AWAIT: ",
+                JSON.stringify(responseObj).substring(0, 200)
+              );
+
+              body = await responseObj.body;
+
+              await client.send("Fetch.fulfillRequest", {
+                requestId,
+                responseCode: 200,
+                responseHeaders,
+                body: responseObj.body,
+              });
+              if (body.length > 1) {
+                // console.log('session: ', session)
+                console.log("url: ", url);
+                console.log(
+                  "PDF responseObj (first 200): ",
+                  JSON.stringify(responseObj).substring(0, 200)
+                );
+                console.log(
+                  "PDF body (first 200): ",
+                  JSON.stringify(body).substring(0, 200)
+                );
+                // resolve(body)
+                res.send(body);
+                await browser.close();
+              }
+              // resolve(body)
+            } else {
+              await client.send("Fetch.continueRequest", { requestId });
+            }
+          });
+        } catch (error) {
+          console.log("ERROR HANDLING CDP SESSION 1: ", JSON.stringify(error));
+        }
+
         const exportButton = await page.waitForXPath(
           "//div[contains(text(),'Export as PDF')]"
         );
@@ -131,51 +210,59 @@ import fs from "fs";
         if (downloadButton) {
           console.log("downloadButton found!");
           await downloadButton.click();
-          // TODO test event: 'popup'
-          // const [response] = await Promise.all([
-          //   page.waitForNavigation(), // This will set the promise to wait for navigation events
-          //   await downloadButton.click(), // After clicking the submi
-          //   // Then the page will be send POST and navigate to target page
-          // ]);
-          // console.log("Promise.all() response: ", response);
         }
 
-        // await page.waitForNavigation({
-        //   waitUntil: "load",
-        // });
+        // try {
+        //   console.log('starting CDP session 2...')
+        //   handleCdpSession(page, 2)
+        // } catch (error) {
+        //   console.log('ERROR HANDLING CDP SESSION 2: ', JSON.stringify(error))
+        // }
+
         await page.waitForTimeout(5000);
 
-        // await page.emulateMediaType("screen");
         const pages = await browser.pages();
 
-        // await page.tab
-        // await page.pdf({ path: "html-page.pdf", format: "A4" });
         console.log("# of pages: ", pages.length);
-        console.log("last page url: ", pages[pages.length - 1].url());
-        // const currPdfUrl = page.url();
-        // console.log("initial page url: ", currPdfUrl);
-        // console.log("targetchanged page url: ", currPageUrl);
+        for (let page of pages) {
+          console.log(`page URL: ${page.url()}`);
+        }
 
-        await page.goto(pages[pages.length - 1].url());
+        await page.goto(pages[pages.length - 1].url(), {
+          waitUntil: "networkidle0",
+        });
+        await page.waitForTimeout(2000);
         // await page.emulateMediaType("screen");
-        // await page.pdf({ path: "html-page.pdf" });
-        const pageContent = await page.content();
+        // try {
+        //   // await page.emulateMediaType('screen')
+        //   await page.pdf({ path: 'html-page.pdf', format: 'A4' })
+        // } catch (error) {
+        //   console.error('error generating pdf: ', JSON.stringify(error))
+        // }
+        pageContent = await page.content();
+        await page.waitForTimeout(2000);
+        // res.send(pageContent)
+        // console.log('pageContent: ', pageContent.substring(0, 150))
+        // res.send(pageContent)
 
-        console.log('pageContent: ', pageContent);
-        res.send(pageContent)
+        //TODO  https://stackoverflow.com/questions/56254177/open-puppeteer-with-specific-configuration-download-pdf-instead-of-pdf-viewer
 
         // const pdfFetch = await fetch(pages[pages.length - 1].url(), {
-        //   credentials: "include",
-        //   headers: {
-        //     "Content-Type": "application/pdf",
-        //   },
-        // });
-        // const pdfData = await pdfFetch.text();
-        // console.log(pdfData.substring(0, 500));
+        //   credentials: 'same-origin',
+        //   // headers: {
+        //   //   "Content-Type": "application/pdf",
+        //   // },
+        // })
+        // const pdfData = await pdfFetch.text()
+        // console.log(pdfData.substring(0, 500))
+        // res.send(pdfData)
+
+        // res.send('done')
       }
     }
 
-    await browser.close();
+    // await browser.close()
+    // res.send(pageContent)
   } catch (error) {
     console.error(error);
     await page.screenshot({ path: "screenshot.png" });
